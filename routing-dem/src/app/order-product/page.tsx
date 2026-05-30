d84f786b289d5f6951d0bb334f753e8925cdc7f7
MarsalShyam <shyam12marsal@gmail.com>
2026-05-30 16:05:54 +0530

    navigating programmatically

---
+++ routing-dem/src/app/order-product/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+//useRouter only works in use client
+import {useRouter} from 'next/navigation';
+export default function OrderProduct() {
+    const router=useRouter();
+    const handleClick=()=>{
+        console.log("Order Placed");
+        // router.push("/");
+        router.replace("/");
+        //push, replace, refresh, back, forward, prefetch, 
+    }; 
+    return (
+        <>
+        <h1>Order Product</h1>
+        <button onClick={handleClick}>Place Order</button>
+        </>
+        
+    );
+}
