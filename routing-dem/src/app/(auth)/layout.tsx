bb56fab8724fa65f4829739777e6a2bb67015ee3
MarsalShyam <shyam12marsal@gmail.com>
2026-05-30 14:56:36 +0530

    link, activelink, navigation understanding

--- routing-dem/src/app/(auth)/layout.tsx
+++ routing-dem/src/app/(auth)/layout.tsx
@@ -1,18 +1,34 @@
+'use client'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-
+const navLinks=[
+  {name:"Register", href:"/register"},
+  {name:"Login",href:"/login"},
+  {name:"Forgot Password", href:"/forgot-password"},
+]
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname=usePathname();
   return (
     <html
       lang="en"
     >
       <body className="">
-        <header style={{backgroundColor:"lightblue",padding:"1rem"}}>
+        {/* <header style={{backgroundColor:"lightblue",padding:"1rem"}}>
           <p>Auth Header</p>
-        </header>
+        </header> */}
+        {navLinks.map((link)=>{
+          const isActive= pathname===link.href||(pathname.startsWith(link.href) && link.href!="/");
+          return(
+            <Link
+            className={isActive?"font-bold mr-4":"text-blue-500 mr-4"}
+            key={link.href} href={link.href}>{link.name}</Link>
+          )
+        })}
         {children}
         <footer style={{backgroundColor:"ghostwhite",padding:"1rem"}}>
           <p>Auth Footer</p>
