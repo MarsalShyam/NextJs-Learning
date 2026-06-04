bc6dca170f341761faf4a68d0e205b0b0cd16fa2
MarsalShyam <shyam12marsal@gmail.com>
2026-06-04 11:52:49 +0530

    Handling Delete Request

--- route-handlers-demo/app/commets/[id]/route.ts
+++ route-handlers-demo/app/commets/[id]/route.ts
@@ -1,6 +1,7 @@
 import {comments} from '../data';
 
-export async function GET(request:Request,
+export async function GET(
+    _request:Request,
     {params}:{params:Promise<{id:string}>}
 ){
     const {id}=await params;
@@ -16,4 +17,15 @@ export async function PATCH(request:Request,{params}:{params:Promise<{id:string}
     const index=comments.findIndex((comment)=>comment.id===parseInt(id));
     comments[index].text=text;
     return Response.json(comments[index]);
+}
+
+export async function DELETE(
+    _request:Request,
+    {params}:{params:Promise<{id:string}>}
+){
+    const {id}=await params;
+    const index=comments.findIndex((comment)=>comment.id===parseInt(id));
+    const deletedComment=comments[index];
+    comments.splice(index,1);
+    return Response.json(deletedComment);
 }
