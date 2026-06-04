2ed7450138ff614e7e0896b1571b9e6a21a36141
MarsalShyam <shyam12marsal@gmail.com>
2026-06-04 14:28:29 +0530

    cookies in route handlers

--- route-handlers-demo/app/profile/api/route.ts
+++ route-handlers-demo/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import {headers} from "next/headers"
+import {headers, cookies} from "next/headers"
 
 // export async function GET(request:NextRequest) {
 //     const requestHeaders=new Headers(request.headers);
@@ -11,7 +11,17 @@ export async function GET(request:NextRequest) {
     const headersList=await headers();
     console.log(headersList.get("Authorization"));
 
+    const theme=request.cookies.get("theme");
+    console.log(theme);
+
+    const cookieStore=await cookies();
+    cookieStore.set("resultPerPage","20");
+    console.log(cookieStore.get("resultPerPage"));
+
     return new Response("<h1>Profile API data</h1>",{
-        headers:{"Content-Type":"text/html"},
+        headers:{
+            "Content-Type":"text/html",
+            "Set-Cookie":"theme=dark",
+        },
     });
 }
