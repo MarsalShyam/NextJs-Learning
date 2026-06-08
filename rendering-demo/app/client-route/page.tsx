ebac90618f803593a81c3dcc8a3a8568f3f10c35
MarsalShyam <shyam12marsal@gmail.com>
2026-06-08 14:59:54 +0530

    context providers

--- rendering-demo/app/client-route/page.tsx
+++ rendering-demo/app/client-route/page.tsx
@@ -7,9 +7,12 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 // import './ImageSlider.css';
 
+import { useTheme } from '@/components/theme-provider';
+
 export default function ClientRoutePage() {
     // const result=serverSideFunction();
 
+    const theme=useTheme()
     const settings = {
     dots: true,
   };
@@ -17,7 +20,7 @@ export default function ClientRoutePage() {
     return(
         <>
         {/* <h1>Client Route Page {result}</h1> */}
-        <h1>Client Route Page</h1>
+        <h1 style={{color:theme.colors.primary}}>Client Route Page</h1>
         <div className="image-slider-container">
       <Slider {...settings}>
         <div>
