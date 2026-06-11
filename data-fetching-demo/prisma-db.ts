658ae0dd9c6bb6bbc5188fc279e5eaf49d011517
MarsalShyam <shyam12marsal@gmail.com>
2026-06-11 13:57:13 +0530

    Fetching data from db and data mutation

---
+++ data-fetching-demo/prisma-db.ts
@@ -0,0 +1,69 @@
+import { PrismaClient } from "./app/generated/prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+// const adapter=new PrismaBetterSQlite3({url:"file:./dev.db"})
+const adapter = new PrismaBetterSqlite3({ url: "file:./dev.db" });
+
+// const prisma = new PrismaClient();
+const prisma = new PrismaClient({adapter});
+
+const seedProducts=async()=>{
+    const count=await prisma.product.count();
+    if(count==0){
+        await prisma.product.createMany({
+            data:[
+                {title:"Product 1", price:500, description:"Description 1"},
+                {title:"Product 2", price:700, description:"Description 2"},
+                {title:"Product 3", price:900, description:"Description 3"},
+            ]
+        })
+    }
+}
+
+//Run seed if needed
+seedProducts()
+
+//CRUD operation
+
+export async function getProducts(){
+    await new Promise((resolve)=> setTimeout(resolve,1500));
+    return prisma.product.findMany();
+}
+
+export async function getProduct(id:number){
+    await new Promise((resolve)=>setTimeout(resolve,1500));
+    return prisma.product.findUnique({
+        where:{id},
+    });
+}
+
+export async function addProduct(
+    title:string,
+    price:number,
+    description:string,
+){
+    await new Promise((resolve)=> setTimeout(resolve, 1500));
+    return prisma.product.create({
+        data:{title, price, description},
+    });
+}
+
+export async function updateProduct(
+    id:number,
+    title:string,
+    price:number,
+    description:string,
+){
+    await new Promise((resolve)=>setTimeout(resolve,1500));
+    return prisma.product.update({
+        where:{id},
+        data:{title, price, description},
+    })
+};
+
+export async function deleteProduct(id:number){
+    await new Promise((resolve)=>setTimeout(resolve, 1500));
+    return prisma.product.delete({
+        where:{id},
+    });
+}
