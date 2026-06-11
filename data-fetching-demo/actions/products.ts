0510933704818881482e8dc720e3863939eebf2b
MarsalShyam <shyam12marsal@gmail.com>
2026-06-11 19:34:56 +0530

    Update server action

--- data-fetching-demo/actions/products.ts
+++ data-fetching-demo/actions/products.ts
@@ -1,5 +1,5 @@
 "use server"
-import { addProduct } from "@/prisma-db";
+import { addProduct, updateProduct } from "@/prisma-db";
 import { redirect } from "next/navigation";
 
 export type Errors = {
@@ -12,20 +12,42 @@ export type FormState = {
   errors: Errors;
 }
 
-export async function createProduct(prevState:FormState, formData: FormData) {
-    const title = formData.get("title") as string;
-    const price = formData.get("price") as string;
-    const description = formData.get("description") as string;
+export async function CreateProduct(prevState: FormState, formData: FormData) {
+  const title = formData.get("title") as string;
+  const price = formData.get("price") as string;
+  const description = formData.get("description") as string;
 
-    const errors: Errors = {};
-    if (!title) { errors.title = "Title is required"; }
-    if (!price) { errors.price = "Price is required"; }
-    if (!description) { errors.description = "Description is required" };
+  const errors: Errors = {};
+  if (!title) { errors.title = "Title is required"; }
+  if (!price) { errors.price = "Price is required"; }
+  if (!description) { errors.description = "Description is required" };
 
-    if (Object.keys(errors).length > 0) {
-      return { errors };
-    }
-
-    await addProduct(title, parseInt(price), description);
-    redirect("/products-db");
+  if (Object.keys(errors).length > 0) {
+    return { errors };
   }
+
+  await addProduct(title, parseInt(price), description);
+  redirect("/products-db");
+}
+
+export async function editProduct(
+  id: number,
+  prevState: FormState,
+  formData: FormData
+) {
+  const title = formData.get("title") as string;
+  const price = formData.get("price") as string;
+  const description = formData.get("description") as string;
+
+  const errors: Errors = {};
+  if (!title) { errors.title = "Title is required"; }
+  if (!price) { errors.price = "Price is required"; }
+  if (!description) { errors.description = "Description is required" };
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  await updateProduct(id,title, parseInt(price), description);
+  redirect("/products-db");
+}
